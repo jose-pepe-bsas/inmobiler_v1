06a5583d5937d056d0f7f627ef8e2ea44f11ca73
jose <jose.s.contacto@gmail.com>
2024-12-17 15:08:56 -0300

    wip(agreement.proposal): proposal dto is created with required parameters

---
+++ test/help/proposal_factory.js
@@ -0,0 +1,62 @@
+const rate_id = 12421421;
+const rate_type = "VARIABLE";
+const variation_lapse = 3;
+const variation_index = "IPC";
+
+const rate = {
+  rate_id,
+  type: rate_type,
+  variation_lapse,
+  variation_index
+};
+
+const deposit_type = "as_percent_total_rent";
+const deposit_value = 12555;
+
+const deposit = {
+  type: deposit_type,
+  value : deposit_value,
+};
+
+const type = "RENT"
+const about = {
+  start_date:"2024/05",
+  end_date:"2025/05",
+  starting_value:40000,
+  rate: rate,
+  deposit: deposit,
+};
+
+const offering = {
+  type,
+  about
+};
+
+const documents = ["loc1","loc2"];
+const offerentID = 32432532;
+const demandID = 3052335;
+const proposed_validity_days = 14;
+const currency = "AR";
+const offeringDTO = offering;
+
+const validProposalDTOData = {
+ documents,
+ offerentID,
+ demandID,
+ proposed_validity_days,
+ currency,
+ offering,
+};
+
+const invalidProposalDTOData = {
+    documents,
+    offerentID,
+    proposed_validity_days,
+    currency,
+    offering,
+};
+
+export {
+  invalidProposalDTOData,
+  validProposalDTOData,
+}
